19d6d601b2cf5a0fd847f096f76afaddc9cc598b
Matheus Torreão <mtorreao1@gmail.com>
2024-11-07 16:46:08 -0300

    chore: other files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProcessModule } from './process/process.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PrismaModule, ProcessModule],
+  imports: [PrismaModule, ProcessModule, EventEmitterModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/process/process.service.ts
+++ src/process/process.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { Transaction as TransactionBase } from 'prisma/generated/base';
 import { PrismaBaseService } from 'src/prisma/prisma.base.service';
+import { WorkService } from './work/work.service';
+import { OnEvent } from '@nestjs/event-emitter';
+import { PrismaBlockchainService } from 'src/prisma/prisma.blockchain.service';
 
-const TRANSACTIONS_BATCH_SIZE = 1000;
+const READ_TRANSACTIONS_BATCH_SIZE = 10000;
+const PROCESS_SIZE = 10;
 
 @Injectable()
 export class ProcessService {
-  constructor(private readonly prismaBaseService: PrismaBaseService) {}
+  constructor(
+    private readonly prismaBaseService: PrismaBaseService,
+    private readonly prismaBlockchainService: PrismaBlockchainService,
+    private readonly workService: WorkService,
+  ) {}
 
   private async *getTransactions(page = 0) {
     const transactions = await this.prismaBaseService.transaction.findMany({
       where: {
-        id: { gt: page * TRANSACTIONS_BATCH_SIZE },
-        synced: false,
+        id: { gt: page * READ_TRANSACTIONS_BATCH_SIZE },
       },
       skip: 0,
-      take: TRANSACTIONS_BATCH_SIZE,
+      take: READ_TRANSACTIONS_BATCH_SIZE,
       orderBy: {
         id: 'asc',
       },
@@ -25,27 +31,37 @@ export class ProcessService {
     yield* this.getTransactions(page + 1);
   }
 
-  private async syncTransactions(transactions: TransactionBase[]) {
-    await this.prismaBaseService.transaction.updateMany({
-      where: {
-        id: { in: transactions.map((t) => t.id) },
-      },
-      data: { synced: true },
-    });
-  }
-
+  @OnEvent('start')
   async handler() {
     console.time('[PROCESS_SERVICE]');
     let totalItens = 0;
-    const syncPromise = [];
+    const totalTransactions = await this.prismaBaseService.transaction.count();
+    const work = await this.workService.handler({
+      limitProcess:PROCESS_SIZE,
+      onMessage: async (length) => {
+        totalItens += length;
+
+        if (totalItens !== totalTransactions) return;
+
+        work.killAll();
+        console.timeEnd('[PROCESS_SERVICE]');
+        console.log(`Total itens processed: ${totalItens}`);
+      },
+    });
+    console.time('[READ_TRANSACTIONS]');
     for await (const transactions of this.getTransactions()) {
-      totalItens += transactions.length;
-      syncPromise.push(this.syncTransactions(transactions));
+      const totalTransactions = transactions.length;
+      const writeTransactionsBatchSize = READ_TRANSACTIONS_BATCH_SIZE / PROCESS_SIZE;
+      for (let i = 0; i < totalTransactions; i += writeTransactionsBatchSize) {
+        work.send(transactions.slice(i, i + writeTransactionsBatchSize));
+      }
     }
-    console.time('[SYNC_TRANSACTIONS]');
-    await Promise.all(syncPromise);
-    console.timeEnd('[SYNC_TRANSACTIONS]');
-    console.timeEnd('[PROCESS_SERVICE]');
-    console.log(`Total itens processed: ${totalItens}`);
+    console.timeEnd('[READ_TRANSACTIONS]');
+  }
+
+  async reset() {
+    console.time('[RESET_TRANSACTIONS]');
+    await this.prismaBlockchainService.transaction.deleteMany();
+    console.timeEnd('[RESET_TRANSACTIONS]');
   }
 }
